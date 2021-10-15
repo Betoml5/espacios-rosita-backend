135929ba461ce256aa0cd69ea6897281152dff48
Beto <betoml5@hotmail.com>
2021-10-15 18:53:20 -0500

    Dummy change

--- components/Report/model.js
+++ components/Report/model.js
@@ -3,9 +3,16 @@ const Schema = mongoose.Schema;
 
 const Report = new Schema({
   name: { type: String },
-  lat: { type: Number, required: true },
-  lng: { type: Number, required: true },
-  type: [{ type: String, required: true }],
+  age: { type: Number },
+  lat: { type: Number },
+  lng: { type: Number },
+  information: String,
+  bullyTypes: [
+    {
+      description: String,
+      value: { type: Boolean, default: false },
+    },
+  ],
 });
 
 module.exports = mongoose.model("Report", Report);
