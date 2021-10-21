c138f92cec66604cea63161dc9a4b50ddafcad70
Betoml5 <betoml5@hotmail.com>
2021-10-21 13:00:20 -0500

    Add adress to report model

--- components/Report/model.js
+++ components/Report/model.js
@@ -4,6 +4,10 @@ const Schema = mongoose.Schema;
 const Report = new Schema({
   name: { type: String },
   age: { type: Number },
+  street: { type: String },
+  neighborhood: { type: String },
+  city: { type: String },
+  street: { type: String },
   lat: { type: Number },
   lng: { type: Number },
   information: String,
